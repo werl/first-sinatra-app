81e77abd43699227b4a13440e604c042a29d62b0
werl <peter@werl.me>
2016-06-14 10:58:42 -0700

    add bcrpyt to gemfile and add page view cookie

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,14 @@
 # Homepage (Root path)
 get '/' do
+  
+
   erb :index
+end
+
+get '/login' do
+   
+end
+
+before do
+  cookies[:page_views] ? cookies[:page_views] = cookies[:page_views].to_i + 1 : cookies[:page_views] = 1
 end
