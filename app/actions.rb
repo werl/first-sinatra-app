8615a78187bd40403b542beb9d3b04ffea1b9b16
werl <peter@werl.me>
2016-06-14 17:27:05 -0700

    got login and user creation working

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,5 @@
 # Homepage (Root path)
+
 get '/' do
   
 
@@ -10,7 +11,22 @@ get '/login' do
 end
 
 post '/login' do
-  "Hello World"
+  user = User.find_by(email: params[:email])
+  puts user.email
+  if user.email == "12345"
+    user.destroy
+    user.save
+  end
+  if !user.nil? && user.password == params[:password]
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login_error'
+  end
+end
+
+get '/login_error' do
+  erb :login_error
 end
 
 get '/create_account' do
@@ -18,7 +34,14 @@ get '/create_account' do
 end
 
 post '/create_account' do
-  "Hello World"
+  if params[:email] && params[:display_name] && params[:password]
+    user = User.new(params)
+    user.password = params[:password]
+    user.save
+    redirect '/login'
+  else
+    redirect '/'
+  end
 end
 
 before do
