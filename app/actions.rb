0171623aa0068503b7cdafa6cd28f1ae2e21c561
werl <peter@werl.me>
2016-06-16 15:45:16 -0700

    more work on login page

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,18 @@
 # Homepage (Root path)
+helpers do
+  def current_user_nil?
+    session[:user_id]
+  end
+
+  def get_current_name
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if @current_user.nil?
+      "Guest"
+    else
+      @current_user.display_name
+    end
+  end
+end
 
 get '/' do
   
@@ -7,33 +21,40 @@ get '/' do
 end
 
 get '/login' do
-  erb :login  
+  if @current_user
+    redirect "/"
+  else
+    erb :login
+  end
+
 end
 
 post '/login' do
   user = User.find_by(email: params[:email])
-  puts user.email
-  if user.email == "12345"
-    user.destroy
-    user.save
-  end
   if !user.nil? && user.password == params[:password]
     session[:user_id] = user.id
     redirect '/'
   else
-    redirect '/login_error'
+    redirect '/login/error'
   end
 end
 
-get '/login_error' do
+get '/login/error' do
   erb :login_error
 end
 
-get '/create_account' do
+get "/logout" do
+  session.clear
+  cookies.clear
+  @current_user = nil
+  redirect "/"
+end
+
+get '/users/new' do
   erb :create_account
 end
 
-post '/create_account' do
+post '/users/new' do
   if params[:email] && params[:display_name] && params[:password]
     user = User.new(params)
     user.password = params[:password]
@@ -44,5 +65,4 @@ post '/create_account' do
   end
 end
 
-before do
-end
+
